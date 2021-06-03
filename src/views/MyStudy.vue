8d11701b6944e7cb6799cc9f373fdbd68a0e9fe2
bobokyung <love2491193@naver.com>
2021-06-03 21:51:21 +0900

    only pagination

--- src/views/MyStudy.vue
+++ src/views/MyStudy.vue
@@ -10,27 +10,31 @@
             <h3 class="mb-0">나의 스터디</h3>
         </b-card-header>
 
-            <el-table class="table-responsive table"
+            <b-table 
                     header-row-class-name="thead-light"
+                    id="studytable"
+                    :current-page="currentPage"
                     @current-change="handleCurrentChange"
-                    :data="projects">
-                <el-table-column label="ID"
+                    :per-page="perPage"
+                    :items="projects">
+
+                <b-tr label="ID"
                                 min-width="100px"
                                 prop="id">
-                </el-table-column>
-                <el-table-column label="Roadmap title"
+                </b-tr>
+                <b-tr label="Roadmap title"
                                 prop="name"
                                 min-width="310px">
-                </el-table-column>
-                <el-table-column label="Status"
+                </b-tr>
+                <b-tr label="Status"
                                 min-width="170px"
                                 prop="status">
-                </el-table-column>
+                </b-tr>
 
-            </el-table>
+            </b-table>
 
             <b-card-footer class="py-4 d-flex justify-content-end">
-                <base-pagination v-model="currentPage" :per-page="10" :total="50"></base-pagination>
+                <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage" aria-controls="studytable"></b-pagination>
             </b-card-footer>
         </b-card>
         </b-col>
@@ -53,83 +57,21 @@
     },
     data() {
       return {
+        perPage: 5,
+        currentPage:1,
         projects : [
-          // {
-          //     title: 'my sql같이 공부해요~@!',
-          //     date:'2020-01-01',
-          //     roadmap_name:'vue.js',
-          //     status: '모집중',
-          //     statusType: 'success',
-          //   },
-          //   {
-              
-          //     title: 'HTML공부 할사람 있나요',
-          //     date:'2020-02-01',
-          //     roadmap_name:'front',
-          //     status: '모집완료',
-          //     statusType: 'info',
-          //   },
-          //   {
-            
-          //     title: 'web sever 공부할사람 모집',
-          //     date:'2020-04-01',
-          //     roadmap_name:'back',
-          //     status: '모집중',
-          //     statusType: 'success',
-          //   },
-          //   {
-            
-          //     title: 'React같이 공부좀 합시다',
-          //     date:'2020-07-01',
-          //     roadmap_name:'backend',
-          //     status: '모집완료',
-          //     statusType: 'info',
-          //   },
-          //   {
-            
-          //     title: 'Vue 스터디',
-          //     date:'2020-08-01',
-          //     roadmap_name:'frontend',
-          //     status: '모집완료',
-          //     statusType: 'info',
-          //   },
-          //   {
           
-          //     title: 'CSS인원 구합니다',
-          //     date:'2020-11-01',
-          //     roadmap_name:'front',
-          //     status: '모집완료',
-          //     statusType: 'info',
-          //   },
-          //   {
-            
-          //     title: 'Devops 토요일에 공부합시다',
-          //     date:'2020-11-01',
-          //     roadmap_name:'dev',
-          //     status: '모집완료',
-          //     statusType: 'info',
-          //   },
-          //   {
-            
-          //     title: 'node.js 초보',
-          //     date:'2020-12-01',
-          //     roadmap_name:'front',
-          //     status: '모집중',
-          //     statusType: 'success',
-          //   },
-          //   {
-            
-          //     title: 'Android',
-          //     date:'2020-11-12',
-          //     roadmap_name:'android',
-          //     status: '모집완료',
-          //     statusType: 'info',
-          //   }
         ]
       };
     },
+    computed: {
+      rows() {
+        return this.projects.length
+      },
+    },
     methods : {
       handleCurrentChange(val){
+        
         this.currentRow = val
         let roadmap_id = this.currentRow.followMap
         let study_id = this.currentRow.id
